e0b1cc20873659838b28fb5757a8edd40b8beefc
Soutaro078 <kkrt235kw@gmail.com>
2025-02-08 14:36:06 +0900

    Station5までクリア

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/station5', function () {
+    return 'Station5';
+});
+
+Route::get('/diary', function () {
+    return 'Hello!';
+});
